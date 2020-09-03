ac9dc6806a860b2aae30d09f7d252710c9c004c6
Panza <gabrielpanzeri@live.com>
2020-09-03 14:30:13 -0300

    reduced code

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 function displayData(){
     let nomeDias = ['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SAB']
     let data = new Date()
-    let dia = data.getDay() - 1
-    let mes = data.getMonth() + 1
-    let ano = data.getFullYear()
     let diaAux = data.getUTCDay()
     let diaSemana = nomeDias[diaAux]
-
-    dia = dia < 10 ? "0" + dia : dia
-    mes = mes < 10 ? "0" + mes : mes
-
-    let dataAtual = diaSemana + ", " + dia + "/" + mes + "/" + ano
+    let dataHoje = data.toLocaleDateString('pt-br')
+    
+    let dataAtual = diaSemana + ", " + dataHoje
 
     document.getElementById('data').innerHTML = dataAtual
 }
@@ -19,15 +14,8 @@ displayData()
 setInterval(displayRelogio, 1000)
 function displayRelogio(){
     let tempo = new Date()
-    let hora = tempo.getHours()
-    let min = tempo.getMinutes()
-    let seg = tempo.getSeconds()
-
-    hora = hora < 10 ? "0" + hora : hora
-    min = min < 10 ? "0" + min : min
-    seg = seg < 10 ? "0" + seg : seg
-
-    let horaAgora = hora + ":" + min + ":" + seg
+    
+    horaAgora = tempo.toLocaleTimeString('pt-BR')
 
     document.getElementById('relogio').innerHTML = horaAgora
 }
